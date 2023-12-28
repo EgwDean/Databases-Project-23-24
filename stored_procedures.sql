9cbd02c19a5e0441b103e0f37a8cb6ddb309a092
Konstantinos Anastasopoulos <101580598+EgwDean@users.noreply.github.com>
2023-12-28 16:30:26 +0200

    Add files via upload
    Για να τρεξει η initialize εχω βαλει σχολιο με τα καταλληλα settings.

---
+++ stored_procedures.sql
@@ -0,0 +1,131 @@
+USE etaireia_aksiologisis;
+
+USE etaireia_aksiologisis;
+
+DELIMITER $
+DROP PROCEDURE IF EXISTS gradeCorrection$
+CREATE PROCEDURE gradeCorrection(IN candidate_username VARCHAR(30))
+BEGIN
+
+DECLARE numLang INT;
+DECLARE numProj INT;
+DECLARE numBSc INT;
+DECLARE numMSc INT;
+DECLARE numPhD INT;
+
+DECLARE corrected_grade INT;
+
+DECLARE evaluator_1 VARCHAR(30);
+DECLARE evaluator_2 VARCHAR(30);
+
+DECLARE grade_1 INT;
+DECLARE grade_2 INT;
+
+SELECT COUNT(*) INTO numLang FROM languages
+WHERE candidate_username=languages.candid;
+
+SELECT COUNT(*) INTO numProj FROM project
+WHERE candidate_username=project.candid;
+
+SELECT COUNT(*) INTO numBSc FROM degree
+INNER JOIN has_degree
+ON degree.titlos=has_degree.degr_title
+AND degree.idryma=has_degree.degr_idryma
+WHERE candidate_username=has_degree.cand_usrname
+AND degree.bathmida='BSc';
+
+SELECT COUNT(*) INTO numMSc FROM degree
+INNER JOIN has_degree
+ON degree.titlos=has_degree.degr_title
+AND degree.idryma=has_degree.degr_idryma
+WHERE candidate_username=has_degree.cand_usrname
+AND degree.bathmida='MSc';
+
+SELECT COUNT(*) INTO numPhD FROM degree
+INNER JOIN has_degree
+ON degree.titlos=has_degree.degr_title
+AND degree.idryma=has_degree.degr_idryma
+WHERE candidate_username=has_degree.cand_usrname
+AND degree.bathmida='PhD';
+
+SET corrected_grade = numLang+numProj+numBSc+2*numMSc+3*numPhD;
+
+SELECT evaluator1 INTO evaluator_1
+FROM evaluation
+WHERE evaluation.evaluated_user=candidate_username;
+
+IF evaluator_1 IS NULL THEN
+UPDATE evaluation
+SET grade1 = corrected_grade
+WHERE evaluation.evaluated_user=candidate_username;
+END IF;
+
+SELECT evaluator2 INTO evaluator_2
+FROM evaluation
+WHERE evaluation.evaluated_user=candidate_username;
+
+IF evaluator_2 IS NULL THEN
+UPDATE evaluation
+SET grade1 = corrected_grade
+WHERE evaluation.evaluated_user=candidate_username;
+END IF;
+
+UPDATE evaluation
+SET final_grade=(grade1+grade2)/2
+WHERE evaluation.evaluated_user=candidate_username;
+
+END$
+DELIMITER ;
+
+DELIMITER $
+DROP PROCEDURE IF EXISTS positionEvaluation$
+CREATE PROCEDURE positionEvaluation(IN ejob_id INT)
+BEGIN
+    DECLARE candidate_username VARCHAR(30);
+    DECLARE evaluator_1 VARCHAR(30);
+    DECLARE evaluator_2 VARCHAR(30);
+    DECLARE finishedFlag INT;
+    
+    DECLARE gradeCorrectionCursor CURSOR FOR
+    SELECT applies.cand_usrname FROM applies
+    WHERE applies.job_id = ejob_id AND applies.state = 'active';
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET finishedFlag = 1;
+    
+    SET finishedFlag = 0;
+    
+    OPEN gradeCorrectionCursor;
+    
+    WHILE (finishedFlag) = 0 DO
+    FETCH gradeCorrectionCursor INTO candidate_username;
+    CALL gradeCorrection(candidate_username);
+    END WHILE;
+    
+    CLOSE gradeCorrectionCursor;
+    
+    SELECT applies.cand_usrname, evaluation.evaluator1, evaluation.evaluator2
+    INTO candidate_username, evaluator_1, evaluator_2
+	FROM applies
+	INNER JOIN evaluation ON applies.cand_usrname = evaluation.evaluated_user
+	WHERE applies.job_id = ejob_id AND applies.state = 'active'
+	ORDER BY evaluation.final_grade DESC, applies.application_date ASC
+    LIMIT 1;
+    
+    DELETE FROM applies
+	WHERE applies.cand_usrname = candidate_username AND applies.job_id = ejob_id;
+    
+    INSERT INTO application_log (e_username, e_evaluator1, e_evaluator2, positionID, a_state, finalGrade)
+	SELECT candidate_username, evaluator_1, evaluator_2, ejob_id, 'completed', evaluation.final_grade
+	FROM evaluation
+	WHERE evaluation.evaluated_user = candidate_username;
+    
+    IF EXISTS (SELECT * FROM has_position WHERE jobid = ejob_id) THEN
+    UPDATE has_position
+	SET employee_username = candidate_username
+	WHERE jobid = ejob_id;
+    ELSE
+    INSERT INTO has_position
+    VALUES (candidate_username, ejob_id);
+    END IF;
+END$
+
+DELIMITER ;
---
+++ triggers.sql
@@ -0,0 +1,115 @@
+USE etaireia_aksiologisis;
+
+DELIMITER $
+DROP TRIGGER IF EXISTS validateApplicationDate$
+CREATE TRIGGER validateApplicationDate
+BEFORE INSERT ON applies
+FOR EACH ROW
+BEGIN
+    DECLARE applicationDate DATE;
+    DECLARE startDate DATE;
+    DECLARE diff INT;
+
+    SELECT start_date INTO startDate
+    FROM job
+    WHERE id = NEW.job_id;
+    
+    SELECT application_date INTO applicationDate
+    FROM applies
+    WHERE applies.application_date=NEW.application_date;
+
+    SET diff = DATEDIFF(startDate, application_date);
+
+    IF diff < 0 OR diff > 15 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Invalid application date! Must be within 15 days before the start date of the job';
+    END IF;
+END$
+DELIMITER ;
+
+DELIMITER $
+DROP TRIGGER IF EXISTS validateApplicationDateCancel$
+CREATE TRIGGER validateApplicationDateCancel
+BEFORE UPDATE ON applies
+FOR EACH ROW
+BEGIN
+    DECLARE applicationDate DATE;
+    DECLARE startDate DATE;
+    DECLARE diff INT;
+
+    SELECT start_date INTO startDate
+    FROM job
+    WHERE id = NEW.job_id;
+
+    SELECT application_date INTO applicationDate
+    FROM applies
+    WHERE applies.application_date=NEW.application_date;
+
+    SET diff = DATEDIFF(startDate, application_date);
+
+    IF diff < 10 AND NEW.state='canceled' THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Invalid application cancel! Must be within 10 days before the start date of the job';
+    END IF;
+END$
+DELIMITER ;
+
+DELIMITER $
+DROP TRIGGER IF EXISTS validateApplicationInsert$
+CREATE TRIGGER validateApplicationInsert
+BEFORE INSERT ON applies
+FOR EACH ROW
+BEGIN
+DECLARE numOfActive INT;
+SELECT COUNT(*) INTO numOfActive
+FROM applies
+WHERE applies.cand_usrname=NEW.cand_usrname
+AND state='active';
+IF numOfActive>=3 THEN
+SIGNAL SQLSTATE '45000'
+SET MESSAGE_TEXT = 'Invalid application. The employee already has at least 3 active applications';
+END IF;
+END$
+DELIMITER ;
+
+DELIMITER $
+DROP TRIGGER IF EXISTS validateApplicationUpdate$
+CREATE TRIGGER validateApplicationUpdate
+BEFORE UPDATE ON applies
+FOR EACH ROW
+BEGIN
+DECLARE numOfActive INT;
+SELECT COUNT(*) INTO numOfActive
+FROM applies
+WHERE applies.cand_usrname=NEW.cand_usrname
+AND state='active';
+IF numOfActive>=3 THEN
+SIGNAL SQLSTATE '45000'
+SET MESSAGE_TEXT = 'Invalid application. The employee already has at least 3 active applications';
+END IF;
+END$
+DELIMITER ;
+
+DELIMITER $
+DROP TRIGGER IF EXISTS evaluationGrade$
+CREATE TRIGGER evaluationGrade
+BEFORE UPDATE ON evaluation
+FOR EACH ROW
+BEGIN
+    DECLARE Grade1 INT;
+    DECLARE Grade2 INT;
+
+    SELECT evaluation.grade1 INTO Grade1
+    FROM evaluation
+    WHERE evaluation.evaluated_user = NEW.evaluated_user;
+
+    SELECT evaluation.grade2 INTO Grade2
+    FROM evaluation
+    WHERE evaluation.evaluated_user = NEW.evaluated_user;   
+
+    IF (Grade1 < 1 OR Grade1 > 20) OR (Grade2 < 1 OR Grade2 > 20) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'The grade has to be between 1 and 20.';
+    END IF;
+END$
+DELIMITER ;
