4ea69d096053eb81eea811c7cda4ad5e1c12e541
Filippos251 <133066095+Filippos251@users.noreply.github.com>
2024-01-05 16:43:12 +0200

    Create TRIGGER(job_delete).sql

---
+++ TRIGGERS/TRIGGER(job_delete).sql
@@ -0,0 +1,14 @@
+DELIMITER $
+DROP TRIGGER IF EXISTS job_delete$
+CREATE TRIGGER job_delete AFTER DELETE ON job
+FOR EACH ROW 
+BEGIN 
+DECLARE job_id INT(11);
+SELECT OLD.job.id INTO job_id FROM job;
+DECLARE current_datetime DATETIME ;
+SET current_datetime = NOW();
+DECLARE event TEXT;
+SET event = CONCAT('The job with id ', job_id, ' has just been deleted by the administrator ',NEW.active_admin.username);
+INSERT INTO administrator_log VALUES (NEW.active_admin.username, current_datetime, event);   #χρησιμοποιείται το ψευδώνημο NEW γιατί δεν με ενδιαφέρει κάποια τίμη που διαγράφεται  
+END$
+DELIMITER;
