d1f6dcb94edef66cf089cf2d19eba34b46a2a479
Filippos251 <133066095+Filippos251@users.noreply.github.com>
2024-01-05 19:12:11 +0200

    Update Trigger(job_delete).sql

--- 3.1.4.1/Trigger(job_delete).sql
+++ 3.1.4.1/Trigger(job_delete).sql
@@ -9,6 +9,6 @@ DECLARE current_datetime DATETIME ;
 SET current_datetime = NOW();
 DECLARE event TEXT;
 SET event = CONCAT('The job with id ', job_id, ' has just been deleted by the administrator ',NEW.active_admin.username);
-INSERT INTO administrator_log VALUES (NEW.active_admin.username, current_datetime, event);   //χρησιμοποιείται το ψευδώνημο NEW γιατί δεν με ενδιαφέρει κάποια τίμη που διαγράφεται  
+INSERT INTO administrator_log VALUES (NEW.active_admin.username, current_datetime, event);   #χρησιμοποιείται το ψευδώνημο NEW γιατί δεν με ενδιαφέρει κάποια τίμη που διαγράφεται  
 END$
 DELIMITER;
